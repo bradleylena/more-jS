c0b0f75c2c0f453e651753ac098252ae7df21fab
bradleylena <kabakabrad@gmail.com>
2025-06-04 03:29:55 +0300

    more js practice

---
+++ script.js
@@ -0,0 +1,25 @@
+alert("Welcome to Our Dealership");
+function demo() {
+  document.getElementById("demo").innerHTML =
+    "<h4>Welcome to Our Dealership practicing jS</h4>";
+  document.getElementById("demo").style = "color:red";
+  // document.getElementById("demo").style = "background-color:yellow";
+}
+
+const cars = ["Mercedes", "BMW", "Audi", "Porsche", "VW"];
+cars[3] = "volkswagen";
+cars.push("Maybach");
+
+function functioncars() {
+  document.getElementById("cars").innerHTML = cars;
+  document.getElementById("cars").style = "color:red";
+  cars.style.left = "200px";
+}
+para.style.transition = "all 0.5s ease";
+para.style.backgroundColor = "red";
+para.style.color = "white";
+para.style.textalign = "center";
+
+function display() {
+  document.getElementById("content").style = "display:block";
+}
