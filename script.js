5cc1696d710707e78cc5812fdc356c59f9bdd680
bradleylena <kabakabrad@gmail.com>
2025-06-05 02:59:15 +0300

    more js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("Welcome to Our Dealership");
+// alert("Welcome to Our Dealership");
 function demo() {
   document.getElementById("demo").innerHTML =
     "<h4>Welcome to Our Dealership practicing jS</h4>";
@@ -23,3 +23,20 @@ para.style.textalign = "center";
 function display() {
   document.getElementById("content").style = "display:block";
 }
+
+let x = 90;
+let y = 100;
+let z = 100 + 500;
+document.getElementById("reda").innerHTML = "total number of cars is " + z;
+console.log(z);
+
+console.log(--x);
+console.log(10 > 5 ? "Greater" : "Smaller");
+
+let age = 18;
+let canvote = age >= 18 ? "Yes" : "No";
+document.getElementById("vote").innerHTML = canvote;
+
+let score = 90;
+let grade = score >= 90 ? "A" : score >= 80 ? "B" : score >= 70 ? "C" : "F";
+console.log(grade);
